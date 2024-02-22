18c613619daed08adbb384fee8f193fb0190d496
bogdandiaconupv <bogdan.diaconupv@gmail.com>
2024-02-22 21:02:29 +0100

    to be continued

--- src/Canva.jsx
+++ src/Canva.jsx
@@ -1,19 +1,23 @@
 import React from 'react';
 import { Canvas, useLoader } from '@react-three/fiber';
 import { useSpring, animated } from '@react-spring/web';
-import { TextureLoader } from 'three';
+import * as THREE from 'three'; // Import THREE from three package
+import earth from './assets/earth3.jpg';
+import cloud from './assets/clouds.jpg'
 
 const Canva = () => {
-    const [color] = useLoader(TextureLoader, '/assets/earth.gif');
+    const color = new THREE.TextureLoader().load(earth)
+    const clouds = new THREE.TextureLoader().load(cloud)
 
     return (
         <Canvas>
-            <ambientLight intensity={2} />
-            <mesh> 
-                <sphereGeometry args={[1, 64, 64]} />
-                <meshStandardMaterial map={color} />
-            </mesh>
-        </Canvas>
+    <ambientLight intensity={0.6} />
+    <directionalLight intensity={3.5} position={[1 , 0, .25]} />
+    <mesh scale={1.6} > 
+        <sphereGeometry args={[1, 64, 64]} />
+        <meshStandardMaterial map={color} aoMap={clouds}/>
+    </mesh>
+</Canvas>
     );
 };
 
